942775647a957a2cf87f834d41cc1fbde4744f5c
pgmanutd <prashantmanunited@gmail.com>
2019-04-26 12:15:09 +0530

    fix(utils): replace import.meta.url with __dirname in getDirname.ts
    - import.meta.url was not working properly on windows.
    - Add TODO to replace it with 'require('url').fileURLToPath(import.meta.url)' when upgraded to node 11

--- src/utils/getDirname.ts
+++ src/utils/getDirname.ts
@@ -1,6 +1,5 @@
-import path from 'path';
-import { URL } from 'url';
-
-const getDirname = () => path.dirname(new URL(import.meta.url).pathname);
+// TODO: Replace __dirname with require('url').fileURLToPath(import.meta.url);
+// when upgraded to node 11
+const getDirname = () => __dirname;
 
 export default getDirname;
