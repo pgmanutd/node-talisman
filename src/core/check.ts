38fc9b551da25e9cf728210a104b9276e2be5e4d
pgmanutd <prashantmanunited@gmail.com>
2019-04-26 16:12:32 +0530

    fix(binary file): wrap binary file path in quotes when not on windows
    - In order to support spaces in binary path, wrap it in quotes

--- src/core/check.ts
+++ src/core/check.ts
@@ -5,6 +5,9 @@ import { CONSOLE_COLORS } from '../constants';
 
 import logToConsole, { logToConsoleForDebugging } from '../utils/logToConsole';
 import getProcessArgs from '../utils/getProcessArgs';
+import wrapTextIfNotWindows from '../utils/wrapTextIfNotWindows';
+
+const wrapTextWithQuotesIfNotWindows = wrapTextIfNotWindows(`"`);
 
 const check: ({
   filePath,
@@ -22,25 +25,28 @@ const check: ({
     );
     logToConsole();
 
-    execSh([`${filePath} ${getProcessArgs(args)}`], error => {
-      if (!error) {
+    execSh(
+      [`${wrapTextWithQuotesIfNotWindows(filePath)} ${getProcessArgs(args)}`],
+      error => {
+        if (!error) {
+          logToConsole(
+            { color: CONSOLE_COLORS.green },
+            messages.check.succeed.toString(),
+          );
+          logToConsole();
+
+          return resolve();
+        }
+
         logToConsole(
-          { color: CONSOLE_COLORS.green },
-          messages.check.succeed.toString(),
+          { color: CONSOLE_COLORS.red },
+          messages.check.failed.toString(),
         );
         logToConsole();
 
-        return resolve();
-      }
-
-      logToConsole(
-        { color: CONSOLE_COLORS.red },
-        messages.check.failed.toString(),
-      );
-      logToConsole();
-
-      return reject(error);
-    });
+        return reject(error);
+      },
+    );
   });
 };
 
