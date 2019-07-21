1d03ae457b0202365d5681ec76ab844c4d92639f
pgmanutd <prashantmanunited@gmail.com>
2019-07-21 06:34:35 +0530

    chore(lint-staged): convert .lintstagedrc to .lintstagedrc.js

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  '**/*.ts': filenames => [
+    `eslint --fix ${filenames.join(' ')}`,
+    `git add ${filenames.join(' ')}`,
+    'tsc -p tsconfig.json --noEmit',
+    `cross-env NODE_ENV=test jest --projects ./scripts/jest --bail --findRelatedTests ${filenames.join(
+      ' ',
+    )}`,
+  ],
+};
