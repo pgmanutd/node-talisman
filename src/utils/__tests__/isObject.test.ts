711a28595c05f9061a351f0c8deb02124f920732
pgmanutd <prashantmanunited@gmail.com>
2020-02-20 15:50:59 +0530

    build(npm): upgrade dependencies

--- src/utils/__tests__/isObject.test.ts
+++ src/utils/__tests__/isObject.test.ts
@@ -2,14 +2,14 @@ import isObject from '../isObject';
 
 describe('#isObject', () => {
   test.each`
-    value        | output
-    ${[1, 2]}    | ${true}
-    ${null}      | ${false}
-    ${undefined} | ${false}
-    ${() => {}}  | ${true}
-    ${/ab+c/}    | ${true}
-    ${Object()}  | ${true}
-    ${' '}       | ${false}
+    value              | output
+    ${[1, 2]}          | ${true}
+    ${null}            | ${false}
+    ${undefined}       | ${false}
+    ${() => undefined} | ${true}
+    ${/ab+c/}          | ${true}
+    ${Object()}        | ${true}
+    ${' '}             | ${false}
   `('returns $output for passed $value', ({ value, output }) => {
     expect(isObject(value)).toEqual(output);
   });
