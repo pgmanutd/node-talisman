0aacf6dd79caf16960a899c138399b0f5b00b2d7
pgmanutd <prashantmanunited@gmail.com>
2020-08-10 03:06:54 +0530

    build(tw-talisman): upgrade from 1.3.0 to 1.4.0

--- src/constants.ts
+++ src/constants.ts
@@ -12,26 +12,26 @@ export const CONSOLE_COLORS = {
 export const CHECKSUMS = {
   talisman_linux_386:
     process.env.TALISMAN_LINUX_386_CHECKSUM ||
-    '6074aa68810b098d4b6085623ec3ab48da61363b1bd0feb69b4ef61b8e8c2ed0',
+    'd6d9a807139e89ca2ca21249212edb042ff938a54c87bcb6f8a4469e25bd9f0b',
   talisman_linux_amd64:
     process.env.TALISMAN_LINUX_AMD64_CHECKSUM ||
-    '2489937228e1611a38c49635834fb096a6eaa08a990d35726f0ff470004a90cf',
+    '7c9c0a22b26641bf29ec28b0c6f0250ec84cbb8edfe92b57b765e8b6d964ce16',
   talisman_darwin_386:
     process.env.TALISMAN_DARWIN_386_CHECKSUM ||
-    '4d1cb537a54fff0a0e12a13053d2a2e53406d9c71e7349465051df1dfc5d8185',
+    '34762a9ff4aa68dda820e7637460390096b217e45aa6e677c9af4bee451ea3e8',
   talisman_darwin_amd64:
     process.env.TALISMAN_DARWIN_AMD64_CHECKSUM ||
-    '72ee52207483faa8e630e6ad22493a1f048c50b460c923ef4e93c1f555089eb4',
+    '3a826bcd043c24322cd9cc46b524eb927a6fd7b52a4986c1a8a47c9bfa3ad5e2',
   'talisman_windows_386.exe':
     process.env.TALISMAN_WINDOWS_386_CHECKSUM ||
-    '1ce60b99d29e424af4476a0c265060b25712fed322663f3602a3f038480c8acb',
+    'eecfa334039fdd7590d18f6498a6a1bdbb875539c2777c8bde1ee39f4cebc118',
   'talisman_windows_amd64.exe':
     process.env.TALISMAN_WINDOWS_AMD64_CHECKSUM ||
-    'bfd6f63bf23696a6f92cbee79ae080fa9913fe2b18118c588e8d4510c7a45385',
+    '122c1a761a1e1c4d13c118e7663305424c683151e2afb63a4f1c3b5519c84ecc',
 };
 
 export const META_INFO = {
-  version: process.env.TALISMAN_VERSION || 'v1.3.0',
+  version: process.env.TALISMAN_VERSION || 'v1.4.0',
 };
 
 export const PATHS = {
