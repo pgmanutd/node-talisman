c18384f025291ece0ded9282d6bf581de16c6c3e
pgmanutd <prashantmanunited@gmail.com>
2020-09-23 22:51:43 +0530

    build(tw-talisman): upgrade from 1.5.0 to 1.6.0

--- src/constants.ts
+++ src/constants.ts
@@ -12,25 +12,25 @@ export const CONSOLE_COLORS = {
 export const CHECKSUMS = {
   talisman_linux_386:
     process.env.TALISMAN_LINUX_386_CHECKSUM ||
-    'd9d26274823325bae9fbb4fea89b9dab428e2043073dc6b40aeeb0e0e48b50cf',
+    'b0c8e8c2b6cca55025f84fe91e24ba9a9f6fee8f0aa806aab53c3d16c19f197a',
   talisman_linux_amd64:
     process.env.TALISMAN_LINUX_AMD64_CHECKSUM ||
-    '3a63dd695be3f80d0d605f67510b1e6a1f914f9ff2b0496bd344e3343724a0e7',
+    'a377b6df5c1bc292c74141a301640ab265ffb563f5bcabc10a84f024903b42bd',
   // NOTE: Checksum for darwin_386 not present in original checksum file.
   talisman_darwin_386: process.env.TALISMAN_DARWIN_386_CHECKSUM || '',
   talisman_darwin_amd64:
     process.env.TALISMAN_DARWIN_AMD64_CHECKSUM ||
-    'da84c0af76b32903c27aa17cb94bbc19709903e30160a02b4e49528e83e029f4',
+    '8dd5dcbb7f2117df625dcf5fa4732734bdcd08667ec8fb592373baa9183cb697',
   'talisman_windows_386.exe':
     process.env.TALISMAN_WINDOWS_386_CHECKSUM ||
-    'cb03c3f32e02b7a6ea803c841b8c994faf0659ae1f2e864d0d634dddca01a31d',
+    '1653cc62c1014479253d6ad42ec6cd29ecd4e4cc5346fb4e57664f78cea2a7a3',
   'talisman_windows_amd64.exe':
     process.env.TALISMAN_WINDOWS_AMD64_CHECKSUM ||
-    '168bbefb4ea35edf59dc6552d3fc2e921d8bfc16af3ce899dbbcc3afaa2b8e9a',
+    'b39e3a302b70c3a1df3be15be1345f04d293933457011f17da41ef0f4ce3334e',
 };
 
 export const META_INFO = {
-  version: process.env.TALISMAN_VERSION || 'v1.5.0',
+  version: process.env.TALISMAN_VERSION || 'v1.6.0',
 };
 
 export const PATHS = {
